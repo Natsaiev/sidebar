74820ea2fe6c971f119ff7afc0270ab277d982ab
Natsaiev <natsaievv@gmail.com>
2024-06-18 21:28:52 +0200

    Переделал

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,7 @@
+const sideMenu = document.querySelector('.side-menu');
+const menuToggle = document.querySelector('#menu-toggle');
 
+
+menuToggle.addEventListener('click', () => {
+  sideMenu.classList.toggle('show');
+});
